26fa7adfb0efd4deee341025813c852badeb6267
JABermudez11 <justinanbermudez@gmail.com>
2020-09-12 19:55:23 -0700

    adds almost all features for a game

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -4,8 +4,21 @@ const GamesForm = () => {
     return(
         <form >
             <div>
-                <label>Game Title:</label>
-                <input type="text" name="title" />
+                <div>
+                    <label>Game Title:</label>
+                    <input type="text" name="title" />
+                </div>
+
+                <div>
+                    <label>Game Genre:</label>
+                    <input type="text" name="genre" />
+                </div>
+
+                <div>
+                    <label>Game Rating:</label>
+                    <input type="Number" name="rating" />
+                </div>
+                
                 <input type="submit" value="submit" />
             </div>
         </form>
