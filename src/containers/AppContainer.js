b3353cedf50d38186d1044f0a7d57cb80fe27eaa
JABermudez11 <justinanbermudez@gmail.com>
2020-09-12 17:35:14 -0700

    app container imported

---
+++ src/containers/AppContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AppContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h2>App Container Test</h2>
+            </div>
+        )
+    }
+}
