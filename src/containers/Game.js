f973dde4a183a8c9a1c9545b9752003b64324e35
JABermudez11 <justinanbermudez@gmail.com>
2020-09-19 22:14:47 -0700

    toggle game form

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import GameForm from '../components/GameForm'
 
 export default class Game extends Component {
+
+    state = {
+        formClick: false
+    }
+
+    handleClick = () =>{
+        this.setState({ formClick: !this.state.formClick})
+    }
+
     render() {
         return (
             <div>
-                <h1>Games Page</h1>
+                <h1>Games Page</h1>                
+                <button text="Add Game" onClick={this.handleClick}>Add Game</button>
+                { this.state.formClick ? <GameForm /> : null }
             </div>
         )
     }
