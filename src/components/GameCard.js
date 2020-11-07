abe29e837baf3b9229c4b411ef4316884a8d879f
JABermudez11 <justinanbermudez@gmail.com>
2020-11-06 22:24:05 -0800

    description added to each game

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -6,7 +6,7 @@ const GameCard = (props) => {
         <div>
             {console.log(props)}
             <h1>{props.game.title}</h1>
-            
+            <p>{props.game.description}</p>
         </div>
     )    
 }
