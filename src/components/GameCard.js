3ea42bcb60aea65aca84654229534af27c2256ca
JABermudez11 <justinanbermudez@gmail.com>
2020-11-21 19:48:40 -0800

    labeled genre

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -6,7 +6,7 @@ const GameCard = (props) => {
         <div>
             {console.log(props)}
             <h1>{props.game.title}</h1>
-            <h4>{props.game.genre}</h4>
+            <h4>Genre: {props.game.genre}</h4>
             <p>{props.game.description}</p>
             <p>Game submitted by: N/a</p>            
             <button onClick={() => props.deleteGame(props.game._id)}>DELETE GAME</button>
