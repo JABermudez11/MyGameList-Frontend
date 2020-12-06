ccf2b30733b766f3d5344e4622396ff9acde3035
JABermudez11 <justinanbermudez@gmail.com>
2020-12-05 22:28:40 -0800

    flexbox for games page

--- src/containers/GameLists.js
+++ src/containers/GameLists.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import GameCard from '../components/GameCard'
+import gameLists from '../css/gameLists.css'
 
 export default class GameLists extends Component {    
     
@@ -7,8 +8,7 @@ export default class GameLists extends Component {
     render() {
         
         return (
-            <div>
-                <h1>List of Games</h1>
+            <div className="gameLists">                
                 {this.props.games.map(game => <GameCard key={game._id} game={game} deleteGame={this.props.deleteGame} />)}
             </div>
         )
