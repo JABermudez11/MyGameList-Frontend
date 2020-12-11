29e97e7a4dd6f46b1d735f5bb6340b2b5c6507e0
JABermudez11 <justinanbermudez@gmail.com>
2020-12-11 02:26:15 -0800

    colored game cards with styled components

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
+import { GameCardStyles } from '../css/GameCardStyles';
+
 const GameCard = (props) => {
     
     return (
-        <div>
+        <GameCardStyles>
             {console.log(props)}
             <h1>{props.game.title}</h1>
             <h4>Genre: {props.game.genre}</h4>
             <p>{props.game.description}</p>
             <p>Game submitted by: N/a</p>            
             <button onClick={() => props.deleteGame(props.game._id)}>DELETE GAME</button>
-        </div>
+        </GameCardStyles>
     )    
 }
 
---
+++ src/css/GameCardStyles.js
@@ -0,0 +1,8 @@
+import styles from 'styled-components';
+
+export const GameCardStyles = styles.div`
+    display: flex;
+    flex-direction: column;
+    background-color: purple;
+    border: 4px solid red;
+`;
